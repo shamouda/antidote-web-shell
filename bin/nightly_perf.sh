f140d3a5ec20ed6cebea6bc78521d7b8b51f7aaa
shamouda <ssh.writeme@gmail.com>
2019-08-13 18:22:56 +0200

    nightly perf script fixes

--- bin/nightly_perf.sh
+++ bin/nightly_perf.sh
@@ -1,18 +1,21 @@
 if [ -z "$BENCH_HOME" ]
 then
-      echo "Error: \$BENCH_HOME is empty, set it with the path of the Benchmarks repo"
-      exit
+    echo "Error: \$BENCH_HOME is empty, set it with the path of the Benchmarks repo"
+    exit
 fi
 
-cd $BENCH_HOME
-
-#Read only transaction
-./_build/default/bin/basho_bench examples/antidote_pb.config.ro
-python parse_perf.py 
-python parse_perf.py $BENCH_HOME/tests/current ../public/perf_data 1
+home=$(pwd)
 
 #Weite only transactions
-./_build/default/bin/basho_bench examples/antidote_pb.config.rw
-python parse_perf.py 
-python parse_perf.py $BENCH_HOME/tests/current ../public/perf_data 2
+cd $BENCH_HOME
+./_build/default/bin/basho_bench examples/antidote_pb.config.wo
+cd $home
+python parse_perf.py $BENCH_HOME/tests/current ../public/perf_data 3
+
+#Read only transaction
+cd $BENCH_HOME
+echo "starting read-only transactions"
+./_build/default/bin/basho_bench examples/antidote_pb.config.ro
+cd $home
+python parse_perf.py $BENCH_HOME/tests/current ../public/perf_data 1
 
