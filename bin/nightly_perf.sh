877054fac8b4936b1cdadfd5334c54f8239785ff
shamouda <ssh.writeme@gmail.com>
2019-08-14 13:28:49 +0200

    tests generated in the same script directory

--- bin/nightly_perf.sh
+++ bin/nightly_perf.sh
@@ -6,23 +6,13 @@ then
     exit
 fi
 
-SRC=$(pwd)
-
 #Write only transactions
-cd $BENCH_HOME
 echo "starting write-only transactions"
 $BENCH_HOME/_build/default/bin/basho_bench $BENCH_HOME/examples/antidote_pb.config.wo
-if [ ! -f $BENCH_HOME/tests/current ]; then
-	echo "WARNING: Benchmarking output folder does not exist"
-fi
-python $SRC/parse_perf.py $BENCH_HOME/tests/current 3
+python parse_perf.py tests/current 3
 
 #Read only transaction
-cd $BENCH_HOME
 echo "starting read-only transactions"
 $BENCH_HOME/_build/default/bin/basho_bench $BENCH_HOME/examples/antidote_pb.config.ro
-if [ ! -f $BENCH_HOME/tests/current ]; then
-	echo "WARNING: Benchmarking output folder does not exist"
-fi
-python $SRC/parse_perf.py $BENCH_HOME/tests/current 1
+python parse_perf.py tests/current 1
 
